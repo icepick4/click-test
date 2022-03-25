785092937f4e5c9f736a5e216b67487abcd397dd
Icepick <remi.jara@laposte.net>
2022-03-25 23:05:01 +0100

    pylint maj

--- main.py
+++ main.py
@@ -1,78 +1,74 @@
-import pygame
-from pygame.locals import *
+"""jeu de cps"""
 from time import time
-pygame.init()
+import pygame
+
+pygame.display.init()
 
 #set the window
-windowSize = pygame.display.get_desktop_sizes()[0]
-screen = pygame.display.set_mode(windowSize)
+window_size = pygame.display.get_desktop_sizes()[0]
+screen = pygame.display.set_mode(window_size)
 
 #gestion des cliques
-clicks = 0
-clicksFont = pygame.font.SysFont("Andalé Mono",50)
-clicksSurface = clicksFont.render("CLICKS : {0}".format(clicks),True,(0,0,0))
-clicksRect = clicksSurface.get_rect(midtop=(windowSize[0]/2,windowSize[1]/2))
+CLICKS = 0
+click_font = pygame.font.SysFont("Andalé Mono",50)
+click_surface = click_font.render(f"CLICKS : {CLICKS}",True,(0,0,0))
+click_rect = click_surface.get_rect(midtop=(window_size[0]/2,window_size[1]/2))
 
 #gestion du temps et du départ de la partie
-timer = 10
+TIMER = 10
 start = time()
-timerFont = pygame.font.SysFont("Andalé Mono",50)
-timerSurface = timerFont.render("TIMER : {0}s".format(timer), True,(0,0,0))
-timerRect = timerSurface.get_rect(midbottom=(windowSize[0]/2,windowSize[1]/2))
-playing = True
-partyStart = False
+timer_font = pygame.font.SysFont("Andalé Mono",50)
+timer_surface = timer_font.render(f"TIMER : {TIMER}s", True,(0,0,0))
+timer_rect = timer_surface.get_rect(midbottom=(window_size[0]/2,window_size[1]/2))
+PLAYING = True
+PARTYSTART = False
 
 #gestion de l'affichage du score
-score = 0
-scoreFont = pygame.font.SysFont("Andalé Mono", 50)
-scoreSurface = scoreFont.render("Score : {} CPS".format(score), True, (0,0,0))
-scoreRect = scoreSurface.get_rect(midtop=(-100,-100))
+SCORE = 0
+score_font = pygame.font.SysFont("Andalé Mono", 50)
+score_surface = score_font.render(f"Score : {SCORE} CPS", True, (0,0,0))
+score_rect = score_surface.get_rect(midtop=(-100,-100))
 
 #bouton pour fermer la fenetre
-endFont = pygame.font.SysFont("Andalé Mono", 50)
-endSurface = endFont.render("CLOSE", True, (0,0,0))
-endRect = endSurface.get_rect(topright=(windowSize[0]-10,10))
+end_font = pygame.font.SysFont("Andalé Mono", 50)
+end_surface = end_font.render("CLOSE", True, (0,0,0))
+end_rect = end_surface.get_rect(topright=(window_size[0]-10,10))
 
-while playing:
+while PLAYING:
     for event in pygame.event.get():
-        if event.type == QUIT:
-            playing = False
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 :
+        if event.type == 256:
+            PLAYING = False
+        elif event.type == 5and event.button == 1 :
             #si la partie n'a pas encore débuté
-            if windowSize[0]-140 < event.pos[0] < windowSize[0] and 10 < event.pos[1] < 40:
-                playing = False
-            if clicks == 0:
-                partyStart = True
+            if window_size[0]-140 < event.pos[0] < window_size[0] and 10 < event.pos[1] < 40:
+                PLAYING = False
+            if CLICKS == 0:
+                PARTYSTART = True
                 start = time()
             #sinon on incrémente les cliques et son affichage
-            clicks+=1
-            clicksSurface = clicksFont.render("CLICKS : {0}".format(clicks),True,(0,0,0))
+            CLICKS+=1
+            click_surface = click_font.render(f"CLICKS : {CLICKS}",True,(0,0,0))
 
     #si le chrono est dépassé, on affiche le résultat
-    if timer <= 0:
-        score = clicks/10
-        scoreSurface = scoreFont.render("Score : {} CPS".format(score), True, (0,0,0))
-        scoreRect = scoreSurface.get_rect(topleft=(0,0))
+    if TIMER <= 0:
+        SCORE = CLICKS/10
+        score_surface = score_font.render(f"Score : {SCORE} CPS", True, (0,0,0))
+        score_rect = score_surface.get_rect(topleft=(0,0))
         #les deux états qui définissent le début d'une partie ou non
-        clicks = 0
-        partyStart = False
-    if partyStart:
-        timer = 10 - (time() - start)
+        CLICKS = 0
+        PARTYSTART = False
+    if PARTYSTART:
+        TIMER = 10 - (time() - start)
     else:
-        timer = 10
+        TIMER = 10
 
     #affichage du timer en temps réel
-    timerSurface = timerFont.render("TIMER : {0}s".format(round(timer,1)), True,(0,0,0))
+    timer_surface = timer_font.render(f"TIMER : {round(TIMER,1)}s", True,(0,0,0))
     #on rempli l'écran avec les surfaces : clicks, timer, score
     screen.fill((255,255,255))
-    screen.blit(clicksSurface,clicksRect)
-    screen.blit(timerSurface,timerRect)
-    screen.blit(scoreSurface,scoreRect)
-    screen.blit(endSurface,endRect)
+    screen.blit(click_surface,click_rect)
+    screen.blit(timer_surface,timer_rect)
+    screen.blit(score_surface,score_rect)
+    screen.blit(end_surface,end_rect)
     pygame.display.flip()
-pygame.quit()
-
-
-
-
-
+pygame.display.quit()
